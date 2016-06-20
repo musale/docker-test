047b25c9852a942faaaad29d6036677decf6c1a3
Martin Musale <martinmshale@gmail.com>
2016-06-20 12:03:22 +0300

    update: added list display and filter values

--- core/admin.py
+++ core/admin.py
@@ -5,23 +5,29 @@ from core.models import Broker, GarbagePoint, Cost, Collection
 
 class BrokerAdmin(admin.ModelAdmin):
     empty_value_display = '-empty-'
-    list_display = ('name', 'contact', 'payment_status', 'amount')
+    list_display = ('name', 'contact')
+    list_filter = ('name', 'contact')
 
 
 class GarbagePointAdmin(admin.ModelAdmin):
     empty_value_display = '-empty-'
     list_display = (
-        'location', 'type_of_location', 'person_in_charge', 'contacts', 'paper_size', 'created_at', 'updated_at')
+        'location', 'type_of_location', 'person_in_charge', 'contacts', 'paper_size', 'number_of_papers', 'updated_at')
+    list_filter = (
+        'created_at', 'updated_at', 'location', 'type_of_location', 'person_in_charge', 'contacts', 'paper_size',
+        'number_of_papers')
 
 
 class CostAdmin(admin.ModelAdmin):
     empty_value_display = '-empty-'
     list_display = ('name', 'description', 'amount', 'created_at', 'updated_at')
+    list_filter = ('name', 'description', 'amount', 'created_at', 'updated_at')
 
 
 class CollectionAdmin(admin.ModelAdmin):
-    empty_value_display = '-empty-'
-    list_display = ('collection_area', 'broker', 'created_at', 'updated_at')
+    empty_value_display = '(None)'
+    list_display = ('collection_area', 'broker', 'payment_status', 'amount', 'created_at', 'updated_at')
+    list_filter = ('created_at', 'updated_at', 'collection_area', 'broker', 'payment_status', 'amount')
 
 
 admin.site.register(Broker, BrokerAdmin)
