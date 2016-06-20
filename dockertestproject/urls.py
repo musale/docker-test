5ba1a4f9d9767b89f98fce41a8eaa1a7e56959c2
Martin Musale <martinmshale@gmail.com>
2016-06-20 22:16:16 +0300

    added urls for my apps

--- dockertestproject/urls.py
+++ dockertestproject/urls.py
@@ -13,14 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+
+from core.views import dashboard, CustomLoginView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', admin.site.urls),
+    # url(r'', CustomLoginView.as_view(), name='index'),
+    url(r'^dashboard/', dashboard, name='dashboard'),
+    url(r'', include('registration.backends.default.urls')),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
