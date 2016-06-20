da00ee783f584252e3bd15f0a2298188832b76b2
Martin Musale <martinmshale@gmail.com>
2016-06-20 22:18:31 +0300

    update: view for dashboard

--- core/views.py
+++ core/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'welcome': 'welcome'})
+
+
+class CustomLoginView(TemplateView):
+    template_name = 'registration/login.html'
+
+    def get(self, request, *args, **kwargs):
+        # display dashboard if user is authenticated
+        if request.user.is_authenticated():
+            return dashboard(request)
+        return super(CustomLoginView, self).get(request, args, kwargs)
+
