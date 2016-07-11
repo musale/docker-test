ccd850ffda04279ae5286bc49c4d5555638afb2f
Martin Musale <martinmshale@gmail.com>
2016-07-11 22:20:03 +0300

    update: forms to update and create an area

--- core/forms.py
+++ core/forms.py
@@ -66,3 +66,36 @@ class CreateCostForm(ModelForm):
     def clean(self):
         cleaned_data = super(CreateCostForm, self).clean()
         return cleaned_data
+
+
+class AreasForm(ModelForm):
+    class Meta:
+        model = GarbagePoint
+        fields = '__all__'
+
+
+class AreaUpdateForm(ModelForm):
+    class Meta:
+        model = GarbagePoint
+        fields = '__all__'
+
+    def _get_validation_exclusions(self):
+        exclude = super(AreaUpdateForm, self)._get_validation_exclusions()
+        return exclude
+
+
+class CreateAreaForm(ModelForm):
+    def __init__(self, data=None, files=None, auto_id='id_%s', prefix=None, initial=None, error_class=ErrorList,
+                 label_suffix=None, empty_permitted=False, instance=None, user=None):
+        self.user = user
+        super(CreateAreaForm, self).__init__(data, files, auto_id, prefix, initial, error_class, label_suffix,
+                                          empty_permitted, instance)
+
+    class Meta:
+        model = GarbagePoint
+        fields = '__all__'
+
+    def clean(self):
+        cleaned_data = super(CreateAreaForm, self).clean()
+        return cleaned_data
+
