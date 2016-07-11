18e9cc1e2e8a11b2660d223f34c36fdc11b1d2e8
Martin Musale <martinmshale@gmail.com>
2016-07-11 21:47:24 +0300

    update: added cost table

--- core/tables.py
+++ core/tables.py
@@ -15,3 +15,18 @@ class CollectionsTable(tables.Table):
         attrs = {"class": "table table-striped table-bordered table-sm"}
         sequence = ("selection",)
         exclude = ['id']
+
+
+class CostTable(tables.Table):
+    selection = tables.CheckBoxColumn(accessor="pk",
+                                      orderable=False,
+                                      attrs={"th__input": {"onclick": "toggle(this)"}},
+                                      )
+
+    edit = tables.LinkColumn('update-cost', kwargs={"pk": tables.A("pk")}, orderable=False, empty_values=())
+
+    class Meta:
+        model = Cost
+        attrs = {"class": "table table-striped table-bordered table-sm"}
+        sequence = ("selection",)
+        exclude = ['id']
