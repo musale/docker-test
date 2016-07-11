9a59788a8dcaab4aa9b955340794fc5173e6a46a
Martin Musale <martinmshale@gmail.com>
2016-07-11 08:32:08 +0300

    toggle checkbox function

--- core/static/js/account.js
+++ core/static/js/account.js
@@ -1,3 +1,15 @@
 /**
  * Created by musale on 7/10/16.
  */
+function toggle(source) {
+    checkboxes = document.getElementsByName('selection');
+    $("button[type='submit']").attr("disabled", !source.checked);
+    for(var i in checkboxes)
+        checkboxes[i].checked = source.checked;
+}
+var all_checkboxes = $("input[type='checkbox']"),
+    submitButt = $("button[type='submit']");
+
+all_checkboxes.click(function() {
+    submitButt.attr("disabled", !all_checkboxes.is(":checked"));
+});
