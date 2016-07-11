1a6c4cd58873d30b298ca15c44b1a9aa540faf2e
Martin Musale <martinmshale@gmail.com>
2016-07-11 08:30:02 +0300

    added new apps, contetxt processors and set login redirect url

--- dockertestproject/settings.py
+++ dockertestproject/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'django_tables2',
+    'bootstrap3',
+    'widget_tweaks'
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -65,7 +68,8 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages'
+                'django.contrib.messages.context_processors.messages',
+                'django.core.context_processors.request'
             ],
         },
     },
@@ -128,4 +132,4 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-LOGIN_REDIRECT_URL = '/dashboard/'
+LOGIN_REDIRECT_URL = '/collections/'
