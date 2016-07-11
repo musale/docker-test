cf11f1a61d7ca49070276e8cae2dfadbfa6f1d43
Martin Musale <martinmshale@gmail.com>
2016-07-11 21:44:29 +0300

    update: added cost form to display, add, update

--- core/forms.py
+++ core/forms.py
@@ -34,3 +34,35 @@ class CreateCollectionForm(ModelForm):
     def clean(self):
         cleaned_data = super(CreateCollectionForm, self).clean()
         return cleaned_data
+
+
+class CostForm(ModelForm):
+    class Meta:
+        model = Cost
+        fields = '__all__'
+
+
+class CostUpdateForm(ModelForm):
+    class Meta:
+        model = Cost
+        fields = '__all__'
+
+    def _get_validation_exclusions(self):
+        exclude = super(CostUpdateForm, self)._get_validation_exclusions()
+        return exclude
+
+
+class CreateCostForm(ModelForm):
+    def __init__(self, data=None, files=None, auto_id='id_%s', prefix=None, initial=None, error_class=ErrorList,
+                 label_suffix=None, empty_permitted=False, instance=None, user=None):
+        self.user = user
+        super(CreateCostForm, self).__init__(data, files, auto_id, prefix, initial, error_class, label_suffix,
+                                          empty_permitted, instance)
+
+    class Meta:
+        model = Cost
+        fields = '__all__'
+
+    def clean(self):
+        cleaned_data = super(CreateCostForm, self).clean()
+        return cleaned_data
